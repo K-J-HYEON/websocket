726b11bdbf4b37c22691d453953e82b290b7f409
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-10-22 00:13:43 +0900

    add controller

---
+++ controller/user.controller.js
@@ -0,0 +1,24 @@
+const User = require("../models/user");
+const userController = {};
+
+userController.saveUser = async (userName, sid) => {
+  // 이미 있는 유저인지 확인
+  const user = await User.find;
+
+  // 없다면 새로 유저정보 만들기
+  if (!user) {
+    user = new User({
+      name: userName,
+      token: sid,
+      online: true,
+    });
+  }
+  // 이미 있는 유저라면 연결정보 token값만 바꿔주자
+  user.token = sid;
+  user.online = true;
+
+  await user.save();
+  return user;
+};
+
+module.exports = userController;
--- utils/io.js
+++ utils/io.js
@@ -1,7 +1,14 @@
+const userController = require("../controller/user.controller");
+
 module.exports = function (io) {
   //io ~~~~
   io.on("connection", async (socket) => {
     console.log("client is connected", socket.id);
+
+    socket.on("login", async (userName, cb) => {
+      // 유저정보를 저장
+      const user = userController.saveUser(userName, socket.id);
+    });
 
     socket.on("disconnect", () => {
       console.log("user is disconnected");
